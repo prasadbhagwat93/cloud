1510882360ded65be2227612460ef7bd53321565
Prasad Bhagwat <prasad@172-16-24-134.DYNAPOOL.NYU.EDU>
2017-12-08 19:52:35 -0500

    project

--- howdy/urls.py
+++ howdy/urls.py
@@ -3,5 +3,13 @@ from howdy import views
 
 urlpatterns = [
     url(r'^search$', views.search),
-    url(r'^index/', views.index)
+    url(r'^index', views.index),
+    url(r'^locations$', views.locations),
+    url(r'^itemManagment', views.items),
+    url(r'^add', views.add_items),
+    url(r'^delete', views.delete_items),
+    url(r'^view', views.view_items),
+    url(r'^login', views.login),
+    url(r'^signup', views.signup),
 ]
+
--- howdy/views.py
+++ howdy/views.py
@@ -3,6 +3,7 @@
 # Create your views here.
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+#from django.shortcuts import render_templete
 from django.views.generic import TemplateView
 from django.shortcuts import HttpResponse
 #from .models import Person
@@ -10,11 +11,14 @@ from django.core.exceptions import *
 import os
 import json
 import requests
-
+import ast
 #from urllib.request import urlopen
 #from django.conf.settings import PROJECT_ROOT
 import boto3
 from django.template import loader, Context
+
+lat = 0
+lon =0
 class HomePageView(TemplateView):
 	def get(self, request, **kwargs):
 
@@ -22,49 +26,94 @@ class HomePageView(TemplateView):
 
 
 def search(request):
+    global lat
+    global lon
     if request.method == 'POST':
-		#file_ = open(os.path.join(PROJECT_ROOT, 'filename'))
         search_id = request.POST.get('textfield', None)
-        template = loader.get_template("map.html")
-        try:
-            client = boto3.client('sqs')
-            queue_url = "https://sqs.us-east-1.amazonaws.com/ajfhjahafjahfjah/messi"
-            response_value = client.receive_message(
-    QueueUrl=queue_url,
-    AttributeNames=[
-        'SentTimestamp'
-    ],
-    MaxNumberOfMessages=1,
-    MessageAttributeNames=[
-        'All'
-    ],
-    VisibilityTimeout=0,
-    WaitTimeSeconds=0
-)
-            message = response_value['Messages'][0]
-            receipt_handle = message['ReceiptHandle']
-            client.delete_message(
-            QueueUrl=queue_url,
-            ReceiptHandle=receipt_handle
-            )
-            tweet_text=message['MessageAttributes']['text']['StringValue']
-            tweet_location = message['MessageAttributes']['twitter']['StringValue'].split(",")
-            loc = ""
-            url = "https://maps.googleapis.com/maps/api/geocode/json?address="
+        if(search_id == ''):
+            send_url = 'http://freegeoip.net/json'
+            r = requests.get(send_url)
+            j = json.loads(r.text)
+            lat = j['latitude']
+            lon = j['longitude']
+            return HttpResponse(str(lat))
 
-            for j in tweet_location:
-                loc = loc+j+"+"
-            url = url+loc+"&key=AIzaSyCQk5ru6Y-42Z1kcsiae_O6oHBnDW_ib5w"
-            map_call = json.loads(requests.get(url).text)
-            lat_long = map_call["results"][0]["geometry"]["location"]
-            #tweet_location
-            #output = template.render({"data":search_id})
-            return HttpResponse(lat_long)
-        except Exception as e:
-            return HttpResponse("lol")
-    else:
-        return render(request, "form.html")
-			#return render_to_response('map.html',{'test':res})
+        else:
+            new_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+search_id+"&key=AIzaSyCqk2xG-5-v6vm4xB8q-4UwuYyNoL-b6L4"
+            ans = requests.get(new_url).json()
+            #ans = json.loads(ans)
+            #at = ans['results'][0]['geometry']['location']['lat']
+            lat = ans["results"][0]["geometry"]["location"]["lat"]
+            lon = ans["results"][0]["geometry"]["location"]["lng"]
+            return HttpResponse()
+
 
 def index(request):
-    return render(request, "form.html")
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users')
+    username = request.POST.get('login', None)
+    password = request.POST.get('password', None)
+    response = table.get_item(Key={'username': username},)
+    item = response['Item']['password']
+    if item == password:
+
+        return render(request, "form.html")
+
+    else:
+        return HttpResponse("wrong passoword")
+
+def locations(request):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users')
+    response = table.get_item(Key={'username': 'fab'},)
+    items = response['Item']['age']
+    item = ast.literal_eval(items)
+    context = {'data':str(item)}
+    return render(request,'locations.html', context)
+
+def items(request):
+    return render(request, "itemManagement.html")
+
+def add_items(request):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users')
+    response = table.get_item(Key={'username': 'fab'},)
+    item = response['Item']['age']
+    item = ast.literal_eval(item)
+    item.append(request.POST.get('textfield', None))
+
+    #items = items+","+request.POST.get('textfield', None)
+    table.update_item(Key={'username': 'fab'},UpdateExpression='SET age = :val1',ExpressionAttributeValues={':val1': str(item)})
+    return HttpResponse(item)
+    #pass
+
+
+def delete_items(request):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users')
+    response = table.get_item(Key={'username': 'fab'},)
+    items = response['Item']['age']
+    item = ast.literal_eval(items)
+    try:
+        item.remove(request.POST.get('textfield', None))
+    except:
+        pass
+    update_item = ""
+    table.update_item(Key={'username': 'fab'},UpdateExpression='SET age = :val1',ExpressionAttributeValues={':val1': str(item)})
+    return HttpResponse("goo")
+
+
+def view_items(request):
+    return HttpResponse("goo")
+
+def login(request):
+   return render(request,'login.html')
+
+def signup(request):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('users')
+    username = request.POST.get('login', None)
+    password = request.POST.get('password', None)
+    list_ = []
+    table.put_item(Item={'username': username,'password': password, 'items':str(list_)})
+    return HttpResponse("successfull")
